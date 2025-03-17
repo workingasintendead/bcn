1142662e3f679e205fc6f69de643c4280fefc9eb
Jackson Ruckar <workingasintendead@gmail.com>
2025-03-17 11:07:01 -0700

    logo replaced - baseline test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className=""
+          src="/bcnPHlogo.png"
+          alt="BCN logo"
+          width={400}
+          height={400}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
