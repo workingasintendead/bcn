e97094f6f3eaeac685397042478057efd8899421
Jackson Ruckar <workingasintendead@gmail.com>
2025-03-24 11:15:57 -0700

    FINALLY got jest and turbopack working properly....

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.test.json' }],
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@testing-library|some-other-library)/)',
+  ],
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ function Home() {
     <div className="grid place-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center gap-[32px]">
         <Image
-          className=""
           src="/bcnPHlogo.png"
           alt="BCN logo"
           width={400}
