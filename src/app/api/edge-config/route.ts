261187233b1201a70487ef60c6d7392151923aad
Jax <161626429+workingasintendead@users.noreply.github.com>
2025-04-07 13:28:35 -0700

    API call adjustment (#6)
    * changed the api call due to change in the json structure on the config storage with new sites being added.
    
    * had already defines section in stores, imported that

--- src/app/api/edge-config/route.ts
+++ src/app/api/edge-config/route.ts
@@ -1,8 +1,21 @@
 import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
+import { Section } from '../../../stores/edge-config-store';
 
-export async function GET() {
-  const sectionsData = await get('sections');
-  const responseData = { sections: sectionsData };
-  return NextResponse.json(responseData);
+type SiteData = {
+  sections?: Section[];
+};
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const site = url.searchParams.get('site') || 'bcn';
+
+  const siteData = await get(site);
+
+  if (siteData && typeof siteData === 'object' && 'sections' in siteData) {
+    const sectionsData = (siteData as SiteData).sections ?? [];
+    return NextResponse.json({ sections: sectionsData });
+  }
+
+  return NextResponse.json({ sections: [] });
 }
